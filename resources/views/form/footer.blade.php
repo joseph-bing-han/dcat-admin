c56923791401952d4ea34a7f4d76f025045679b7
Joseph Han <joseph.bing.han@gmail.com>
2024-04-15 10:12:57 +1200

    修复form 没有submit的bug

--- resources/views/form/footer.blade.php
+++ resources/views/form/footer.blade.php
@@ -5,12 +5,14 @@
 
   <div class="col-md-{{$width['field']}}">
     @if(!empty($buttons['submit']))
+      @php($renderButton=false)
       @if($buttons['view'])
         <div class="btn-group pull-right">
           <button class="btn btn-info submit" onclick="$('#{{$id}}-after-save').val('3');">
             <i class="feather icon-eye"></i> {{ trans('admin.save_and_view') }}
           </button>
         </div>
+        @php($renderButton=true)
       @endif
       @if($buttons['continue_editing'])
         <div class="btn-group pull-right mr-2">
@@ -18,28 +20,34 @@
             <i class="feather icon-save"></i> {{ trans('admin.save_and_edit') }}
           </button>
         </div>
+        @php($renderButton=true)
       @endif
       @foreach($buttons as $button)
         @if($button instanceof  Dcat\Admin\Form\Field\Button)
           {!! $button->render() !!}
+          @php($renderButton=true)
         @elseif($button instanceof  Dcat\Admin\Form\AbstractTool)
           {!! $button->render(1, true) !!}
+          @php($renderButton=true)
         @endif
       @endforeach
 
-      @if(!empty($buttons['back']))
+      @if(!$renderButton)
         <div class="btn-group pull-right mr-2">
+          <button class="btn btn-primary submit">
+            <i class="feather icon-save"></i> {{ trans('admin.save') }}
+          </button>
+        </div>
+      @endif
+
+      @if(!empty($buttons['back']))
+        <div class=" btn-group pull-right mr-2">
           <button type="button" class="btn btn-secondary" onclick="history.back()">
             <i class="feather icon-arrow-left-circle"></i> {{ trans('admin.back') }}
           </button>
         </div>
       @endif
 
-      {{--      @if($checkboxes)--}}
-      {{--        <div class="pull-right d-md-flex" style="margin:10px 15px 0 0; display: none!important;">--}}
-      {{--          {!! $checkboxes !!}--}}
-      {{--        </div>--}}
-      {{--      @endif--}}
       <input type="hidden" id="{{$id}}-after-save" name="after-save">
     @endif
 
