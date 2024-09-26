f03f3d5ca8b68e29fbc04449845b6403084d1208
Joseph Han <joseph.bing.han@gmail.com>
2024-09-26 23:20:11 +1200

    Fix Rate UI bug

--- resources/views/form/rate.blade.php
+++ resources/views/form/rate.blade.php
@@ -7,7 +7,7 @@
         @include('admin::form.error')
 
         <div class="input-group">
-            <input type="number" name="{{$name}}" value="{{ $value }}" class="form-control {{ $class }}" placeholder="0" style="text-align:right;" {!! $attributes !!} />
+            <input type="number" name="{{$name}}" value="{{ $value }}" class="form-control {{ $class }}" {!! $attributes !!} />
             <span class="input-group-append"><span class="input-group-text bg-white">%</span></span>
         </div>
 
