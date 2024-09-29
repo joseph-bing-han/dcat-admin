666df63b82102d8ee172a425da3a749d97b19ff2
Joseph Han <joseph.bing.han@gmail.com>
2024-09-30 11:36:11 +1300

    Change Form divide head layout

--- src/Form/Field/Divide.php
+++ src/Form/Field/Divide.php
@@ -6,19 +6,39 @@ use Dcat\Admin\Form\Field;
 
 class Divide extends Field
 {
+    protected $textClass = 'text-center';
+
     public function __construct($label = null)
     {
         $this->label = $label;
     }
 
+    public function textLeft()
+    {
+        $this->textClass = 'pl-4 text-left';
+        return $this;
+    }
+
+    public function textCenter()
+    {
+        $this->textClass = 'text-center';
+        return $this;
+    }
+
+    public function textRight()
+    {
+        $this->textClass = 'pr-4 text-right';
+        return $this;
+    }
+
     public function render()
     {
-        if (! $this->label) {
+        if (!$this->label) {
             return '<hr/>';
         }
 
         return <<<HTML
-<div class="mt-2 text-center mb-2 form-divider">
+<div class="mt-2 mb-2 form-divider {$this->textClass}">
   <span>{$this->label}</span>
 </div>
 HTML;
